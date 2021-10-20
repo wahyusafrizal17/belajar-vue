d4e62ffc5a12c566620bae29bae295e0a8d040fd
Wahyu Safrizal <49548492+wahyusafrizal17@users.noreply.github.com>
2021-10-20 18:27:41 +0700

    init

---
+++ akunt-api/app/Http/Controllers/TransactionController.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Transaction;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\Response;
+
+class TransactionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $transaction = Transaction::orderBy('time', 'DESC')->get();
+        $response = [
+            'message' => 'List transaction order by time',
+            'data' => $transaction,
+        ];
+
+        return response()->json($response, Response::HTTP_OK);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $Validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'amount' => 'required | numeric',
+            'type' => 'required | in:expense,revenue',
+        ]);
+
+        if($Validator->fails()){
+            return response()->json($Validator->errors(), Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        try{
+            $transaction = Transaction::create($request->all());
+            $response = [
+                'message' => 'Transaction created',
+                'data' => $transaction,
+            ];
+
+            return response()->json($response, Response::HTTP_CREATED);
+
+        } catch (QueryException $e){
+
+            return response()->json([
+                'message' => 'Failed ' . $e->errorInfo
+            ]);
+
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $transaction = Transaction::findOrFail($id);
+        $response = [
+            'message' => 'List transaction by id',
+            'data' => $transaction,
+        ];
+
+        return response()->json($response, Response::HTTP_OK);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $transaction = Transaction::findOrFail($id);
+
+        $Validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'amount' => 'required | numeric',
+            'type' => 'required | in:expense,revenue',
+        ]);
+
+        if($Validator->fails()){
+            return response()->json($Validator->errors(), Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        try{
+            $transaction->update($request->all());
+            $response = [
+                'message' => 'Transaction updated',
+                'data' => $transaction,
+            ];
+
+            return response()->json($response, Response::HTTP_OK);
+
+        } catch (QueryException $e){
+
+            return response()->json([
+                'message' => 'Failed ' . $e->errorInfo
+            ]);
+
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $transaction = Transaction::findOrFail($id);
+
+        try{
+            $transaction->delete();
+            $response = [
+                'message' => 'Transaction deleted',
+                'data' => $transaction,
+            ];
+
+            return response()->json($response, Response::HTTP_OK);
+
+        } catch (QueryException $e){
+
+            return response()->json([
+                'message' => 'Failed ' . $e->errorInfo
+            ]);
+
+        }
+    }
+}
---
+++ akunt-api/routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TransactionController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// Route::get('/transaction', [TransactionController::class, 'index']);
+// Route::get('/transaction/{id}', [TransactionController::class, 'show']);
+// Route::post('/transaction', [TransactionController::class, 'store']);
+// Route::put('/transaction/{id}', [TransactionController::class, 'update']);
+// Route::delete('/transaction/{id}', [TransactionController::class, 'destroy']);
+
+Route::resource('/transaction', TransactionController::class)->except(['create', 'edit']);
